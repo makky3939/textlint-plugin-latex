34518f54eb126ad8c47c4e2de2c067949041c79b
elzup <guild0105@gmail.com>
2018-01-07 18:48:32 +0900

    Add test

--- test/LatexProcessor.js
+++ test/LatexProcessor.js
@@ -1,51 +1,33 @@
+"use strict";
 import assert from "power-assert";
-import LatexProcessor from "../src/LatexProcessor";
+import LatexPlugin from "../src/index";
 import { TextLintCore } from "textlint";
+
 import path from "path";
-describe("LatexProcessor-test", function() {
-  describe("LatexPlugin", function() {
-    let textlint;
-    context("when target file is a Latex", function() {
-      beforeEach(function() {
-        textlint = new TextLintCore();
-        textlint.setupProcessors({
-          LatexProcessor: LatexProcessor
-        });
-        textlint.setupRules({
-          "no-todo": require("textlint-rule-no-todo")
-        });
-      });
-      it("should report error", function() {
-        const fixturePath = path.join(__dirname, "/fixtures/test.html");
-        return textlint.lintFile(fixturePath).then(results => {
-          assert(results.messages.length > 0);
-          assert(results.filePath === fixturePath);
-        });
+describe("LatexPlugin", function() {
+  let textlint;
+  beforeEach(function() {
+    textlint = new TextLintCore();
+    textlint.setupPlugins({ html: LatexPlugin });
+    textlint.setupRules({ "no-todo": require("textlint-rule-no-todo") });
+  });
+  context("when target file is a HTML", function() {
+    it("should report error", function() {
+      const fixturePath = path.join(__dirname, "/test.tex");
+      return textlint.lintFile(fixturePath).then(results => {
+        assert(results.messages.length > 0);
+        assert(results.filePath === fixturePath);
       });
     });
-    context("support file extensions", function() {
-      beforeEach(function() {
-        textlint = new TextLintCore();
-        textlint.setupProcessors({
-          LatexProcessor: LatexProcessor
+  });
+  context("when target is text", function() {
+    it("should report error", function() {
+      return textlint
+        .lintText("TODO: this is todo", ".markdown")
+        .then(results => {
+          assert(results.messages.length === 1);
+          assert(results.filePath === "<markdown>");
         });
-        textlint.setupRules({
-          "no-todo": require("textlint-rule-no-todo")
-        });
-      });
-      it("support {.html, .htm}", function() {
-        const fixturePathList = [
-          path.join(__dirname, "/fixtures/test.html"),
-          path.join(__dirname, "/fixtures/test.htm")
-        ];
-        const promises = fixturePathList.map(filePath => {
-          return textlint.lintFile(filePath).then(results => {
-            assert(results.messages.length > 0);
-            assert(results.filePath === filePath);
-          });
-        });
-        return Promise.all(promises);
-      });
     });
   });
 });
